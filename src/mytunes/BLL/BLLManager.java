94667c971d7f796f41e2d5a406f4a8e12a41e344
Fabio <fabio_moniz_1@hotmail.com>
2017-11-27 11:30:36 +0000

    minor folder renaming

--- src/mytunes/BLL/BLLManager.java
+++ src/mytunes/BLL/BLLManager.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mytunes.gui.DAL;
+package mytunes.BLL;
 
 /**
  *
  * @author Emil Pc
  */
-public class DALManager {
+public class BLLManager {
     
 }
