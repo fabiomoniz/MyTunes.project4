017f2e90704de04f468fb23ceacc24e7b3de12e1
Emil Schütt <emildk11@live.dk>
2017-11-27 11:43:00 +0100

    Made Controllers for most stuff in the GUI and created some fxids for the playlist and songslist

--- src/mytunes/gui/MainWindowController.java
+++ src/mytunes/gui/MainWindowController.java
@@ -11,6 +11,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -22,10 +24,54 @@ import javafx.scene.control.Label;
 public class MainWindowController implements Initializable {
     
     private Label label;
+    @FXML
+    private TableView<?> playList;
+    @FXML
+    private TableView<?> songsList;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void playlistNew(ActionEvent event) {
+    }
+
+    @FXML
+    private void playListEdit(ActionEvent event) {
+    }
+
+    @FXML
+    private void playListDelete(ActionEvent event) {
+    }
+
+    @FXML
+    private void songListNew(ActionEvent event) {
+    }
+
+    @FXML
+    private void songListEdit(ActionEvent event) {
+    }
+
+    @FXML
+    private void songListDelete(ActionEvent event) {
+    }
+
+    @FXML
+    private void songPlay(ActionEvent event) {
+    }
+
+    @FXML
+    private void songLast(ActionEvent event) {
+    }
+
+    @FXML
+    private void songNext(ActionEvent event) {
+    }
+
+    @FXML
+    private void SongVolumeDragDetected(MouseEvent event) {
+    }
     
 }
