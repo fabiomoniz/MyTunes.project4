cd8a121ad90bd17e736975cf4aa3ff8bb936d0a3
Yindoom <yindoom@hotmail.com>
2017-12-08 12:11:33 +0100

    Revert "Merge branch 'master' of https://github.com/fabiomoniz/MyTunes.project4"
    This reverts commit 2f27810b9c3e723a1c2599f65ff63d12cb93a89b, reversing
    changes made to 82b76aac455fb3d9d517209158183e68596fe60b.

--- src/mytunes/gui/PlayListEditController.java
+++ src/mytunes/gui/PlayListEditController.java
@@ -10,21 +10,16 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-<<<<<<< HEAD
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import mytunes.BE.PlayList;
 import mytunes.BE.Song;
 
 
-=======
-import javafx.scene.control.TextField;
->>>>>>> 0c5fc2abbd8a047ec49dbd5f51bb97783abb707b
 
 /**
  * FXML Controller class
  *
-<<<<<<< HEAD
  * @author Yindo
  */
 public class PlayListEditController implements Initializable {
@@ -33,15 +28,6 @@ public class PlayListEditController implements Initializable {
     @FXML
     private TextField txtfieldname;
     private PlayList selectedPlayList;
-=======
- * @author Fábio
- */
-public class PlayListEditController implements Initializable {
-
-    @FXML
-    private TextField txtfieldname;
-
->>>>>>> 0c5fc2abbd8a047ec49dbd5f51bb97783abb707b
     /**
      * Initializes the controller class.
      */
@@ -49,7 +35,6 @@ public class PlayListEditController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-<<<<<<< HEAD
     
     
     public void setModel(SongModel model) {
@@ -65,16 +50,10 @@ public class PlayListEditController implements Initializable {
         model.updatePlayList(selectedPlayList);
         
         ((Node)event.getSource()).getScene().getWindow().hide();
-=======
-
-    @FXML
-    private void buttonsave(ActionEvent event) {
->>>>>>> 0c5fc2abbd8a047ec49dbd5f51bb97783abb707b
     }
 
     @FXML
     private void butotncancel(ActionEvent event) {
-<<<<<<< HEAD
         ((Node)event.getSource()).getScene().getWindow().hide();
     }
  
@@ -84,8 +63,3 @@ public class PlayListEditController implements Initializable {
 
     }
 }   
-=======
-    }
-    
-}
->>>>>>> 0c5fc2abbd8a047ec49dbd5f51bb97783abb707b
