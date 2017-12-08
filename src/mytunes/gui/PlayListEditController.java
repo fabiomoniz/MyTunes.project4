0c5fc2abbd8a047ec49dbd5f51bb97783abb707b
Fabio <fabio_moniz_1@hotmail.com>
2017-12-08 10:16:45 +0000

    test

---
+++ src/mytunes/gui/PlayListEditController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytunes.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Fábio
+ */
+public class PlayListEditController implements Initializable {
+
+    @FXML
+    private TextField txtfieldname;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void buttonsave(ActionEvent event) {
+    }
+
+    @FXML
+    private void butotncancel(ActionEvent event) {
+    }
+    
+}
